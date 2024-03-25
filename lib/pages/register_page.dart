050ce994d9ebafef29a6b11c64169c6050732d5a
Youssef <ykmhabib97@gmail.com>
2024-03-25 17:51:44 +0200

    Implemented Modal Progress HUD

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:scholar_chat_app/pages/login_page.dart';
 import 'package:scholar_chat_app/widgets/custom_button.dart';
 import 'package:scholar_chat_app/widgets/custom_text_field.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
@@ -16,77 +17,92 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   final GlobalKey<FormState> _formKey = GlobalKey();
 
+  String? email, password;
+  bool isLoading = false;
+
   @override
   Widget build(BuildContext context) {
-    String? email, password;
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Form(
-            key: _formKey,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Image.asset('assets/images/scholar.png'),
-                  const Text(
-                    "Scholar Chat",
-                    style: TextStyle(
-                      fontFamily: 'pacifico',
-                      fontSize: 32,
-                    ),
-                  ),
-                  const Text(
-                    "Register",
-                    style: TextStyle(
-                      fontSize: 24,
-                    ),
-                  ),
-                  CustomTextField(
-                    onChanged: (value) {
-                      email = value;
-                    },
-                    labelText: 'Email',
-                    hintText: 'example@domain.com',
-                    keyboardType: TextInputType.emailAddress,
-                    obscureText: false,
-                  ),
-                  CustomTextField(
-                    onChanged: (value) {
-                      return password = value;
-                    },
-                    labelText: 'Password',
-                    hintText: 'Enter at least 7 characters',
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: true,
-                  ),
-                  CustomButton(
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        try {
-                          await registerUser(email!, password!);
-                        } on FirebaseAuthException catch (e) {
-                          showSnackBar(context, e);
-                        } catch (e) {
-                          showSnackBar(context, e);
-                        }
-                      }
-                    },
-                    buttonText: "Register",
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text("Already have an account?"),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context)
-                              .pushReplacementNamed(LoginPage.route);
-                        },
-                        child: const Text("Login here"),
+    return ModalProgressHUD(
+      inAsyncCall: isLoading,
+      child: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/scholar.png'),
+                    const Text(
+                      "Scholar Chat",
+                      style: TextStyle(
+                        fontFamily: 'pacifico',
+                        fontSize: 32,
                       ),
-                    ],
-                  )
-                ],
+                    ),
+                    const Text(
+                      "Register",
+                      style: TextStyle(
+                        fontSize: 24,
+                      ),
+                    ),
+                    CustomTextField(
+                      onChanged: (value) {
+                        email = value;
+                      },
+                      labelText: 'Email',
+                      hintText: 'example@domain.com',
+                      keyboardType: TextInputType.emailAddress,
+                      obscureText: false,
+                    ),
+                    CustomTextField(
+                      onChanged: (value) {
+                        return password = value;
+                      },
+                      labelText: 'Password',
+                      hintText: 'Enter at least 6 characters',
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                    ),
+                    CustomButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          try {
+                            await registerUser(email!, password!);
+                            showSnackBar(
+                              context,
+                              'User registered successfully.',
+                            );
+                          } on FirebaseAuthException catch (e) {
+                            showSnackBar(context, e);
+                          } catch (e) {
+                            showSnackBar(context, e);
+                          }
+                          setState(() {
+                            isLoading = false;
+                          });
+                        }
+                      },
+                      buttonText: "Register",
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text("Already have an account?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .pushReplacementNamed(LoginPage.route);
+                          },
+                          child: const Text("Login here"),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
