344787586bd0f86b7c9a87d7c5cd401dbcb8cfc6
Youssef <ykmhabib97@gmail.com>
2024-03-26 07:16:07 +0200

    Modified message model to include timestamp

--- lib/models/message_model.dart
+++ lib/models/message_model.dart
@@ -1,15 +1,21 @@
 import 'package:scholar_chat_app/constants.dart';
 
 class MessageModel {
+  final String id;
   final String text;
+  final DateTime createdAt;
 
   const MessageModel({
+    required this.id,
     required this.text,
+    required this.createdAt,
   });
 
   factory MessageModel.fromJson(json) {
     return MessageModel(
+      id: json['id'],
       text: json[kMessageText],
+      createdAt: json['createdAt'].toDate() as DateTime,
     );
   }
 }
